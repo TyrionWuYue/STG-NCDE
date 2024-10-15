f5ba90bc09b5d05d54d2bc35cbcb5b272365b646
Yue Wu <62507971+TyrionWuYue@users.noreply.github.com>
2024-10-16 00:58:22 +0800

    Fusion graph worked

--- model/GCN.py
+++ model/GCN.py
@@ -2,10 +2,144 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# 全局图卷积(Node Embedding不变)
-class AGCN(nn.Module):
+
+class AttentionLayer(nn.Module):
+    """
+    Make sure the tensor is permuted to correct shape before attention.
+
+    E.g.
+    - Input shape (batch_size, in_steps, num_nodes, model_dim).
+    - Then the attention will be performed across the nodes.
+    """
+
+    def __init__(self, model_dim, num_heads=8):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.num_heads = num_heads
+
+        self.head_dim = model_dim // num_heads
+
+        self.FC_Q = nn.Linear(model_dim, model_dim)
+        self.FC_K = nn.Linear(model_dim, model_dim)
+        self.FC_V = nn.Linear(model_dim, model_dim)
+
+        self.out_proj = nn.Linear(model_dim, model_dim)
+
+    def forward(self, query, key, value):
+        # Q    (batch_size, ..., tgt_length, model_dim)
+        # K, V (batch_size, ..., src_length, model_dim)
+        batch_size = query.shape[0]
+
+        query = self.FC_Q(query)
+        key = self.FC_K(key)
+        value = self.FC_V(value)
+
+        # Qhead, Khead, Vhead (num_heads * batch_size, ..., length, head_dim)
+        query = torch.cat(torch.split(query, self.head_dim, dim=-1), dim=0)
+        key = torch.cat(torch.split(key, self.head_dim, dim=-1), dim=0)
+        value = torch.cat(torch.split(value, self.head_dim, dim=-1), dim=0)
+
+        key = key.transpose(
+            -1, -2
+        )  # (num_heads * batch_size, ..., head_dim, src_length)
+
+        attn_score = (
+            query @ key
+        ) / self.head_dim**0.5  # (num_heads * batch_size, ..., tgt_length, src_length)
+
+        attn_score = torch.softmax(attn_score, dim=-1)
+        out = attn_score @ value  # (num_heads * batch_size, ..., tgt_length, head_dim)
+        out = torch.cat(
+            torch.split(out, batch_size, dim=0), dim=-1
+        )  # (batch_size, ..., tgt_length, head_dim * num_heads = model_dim)
+
+        out = self.out_proj(out)
+
+        return out
+
+
+class SelfAttentionLayer(nn.Module):
+    def __init__(
+        self, model_dim, feed_forward_dim=64, num_heads=8, dropout=0
+    ):
+        super().__init__()
+
+        self.attn = AttentionLayer(model_dim, num_heads)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(model_dim, feed_forward_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(feed_forward_dim, model_dim),
+        )
+        self.ln1 = nn.LayerNorm(model_dim)
+        self.ln2 = nn.LayerNorm(model_dim)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x, dim=-2):
+        # x: (batch_size, ..., length, model_dim)
+        residual = x
+        out = self.attn(x, x, x)  # (batch_size, ..., length, model_dim)
+        out = self.dropout1(out)
+        out = self.ln1(residual + out)
+
+        residual = out
+        out = self.feed_forward(out)  # (batch_size, ..., length, model_dim)
+        out = self.dropout2(out)
+        out = self.ln2(residual + out)
+
+        return out
+
+
+class GatedFusion(nn.Module):
+    def __init__(self, dim):
+        super(GatedFusion, self).__init__()
+        self.dim = dim
+        
+        # Linear layers for node embeddings
+        self.w_dynamic = nn.Linear(in_features=dim, out_features=dim)  # Dynamic embeddings
+        self.w_static = nn.Linear(in_features=dim, out_features=dim)   # Static embeddings
+        
+        # Transformation matrix for static embeddings
+        self.t = nn.Parameter(torch.zeros(size=(self.dim, self.dim)))
+        nn.init.xavier_uniform_(self.t.data, gain=1.414)
+        
+        self.norm = nn.LayerNorm(dim)
+        
+        # Gates
+        self.w_r = nn.Linear(in_features=dim, out_features=dim)
+        self.u_r = nn.Linear(in_features=dim, out_features=dim)
+
+        self.w_h = nn.Linear(in_features=dim, out_features=dim)
+        self.w_u = nn.Linear(in_features=dim, out_features=dim)
+
+    def forward(self, dynamic_nodevec, static_nodevec):
+        batch_size = dynamic_nodevec.size(0)
+        # Normalize dynamic embeddings
+        dynamic_nodevec = self.norm(dynamic_nodevec)
+        
+        # Process dynamic embeddings
+        dynamic_res = self.w_dynamic(dynamic_nodevec) + dynamic_nodevec
+        
+        # Transform static embeddings
+        static_transformed = static_nodevec + torch.einsum('bnd, dd->bnd', [static_nodevec, self.t])
+        
+        # Gate calculation
+        z = torch.sigmoid(dynamic_res + static_transformed)
+
+        r = torch.sigmoid(self.w_r(static_nodevec) + self.u_r(dynamic_nodevec))
+        h = torch.tanh(self.w_h(static_nodevec) + r * (self.w_u(dynamic_nodevec)))
+
+        # Final result: weighted sum of static and dynamic embeddings
+        res = z * dynamic_nodevec + (1 - z) * h
+
+        return res
+
+
+# Global GCN (fixed node embeddings)
+class GlobalAGCN(nn.Module):
     def __init__(self, dim_in, dim_out, cheb_k, embed_dim):
-        super(AGCN, self).__init__()
+        super(GlobalAGCN, self).__init__()
         self.cheb_k = cheb_k
         self.embed_dim = embed_dim
         self.dim_in = dim_in
@@ -28,6 +162,34 @@ class AGCN(nn.Module):
         return x_gconv
 
 
+# Local GCN (Node Embedding change with input data)
+class LocalAGCN(nn.Module):
+    def __init__(self, dim_in, dim_out, cheb_k, embed_dim):
+        super(LocalAGCN, self).__init__()
+        self.cheb_k = cheb_k
+        self.embed_dim = embed_dim
+        self.dim_in = dim_in
+        self.dim_out = dim_out
+        self.weights = nn.Parameter(torch.FloatTensor(cheb_k*dim_in, dim_out))
+        self.bias = nn.Parameter(torch.FloatTensor(dim_out))
+        nn.init.xavier_uniform_(self.weights)
+        nn.init.constant_(self.bias, 0)
+
+    def forward(self, x, node_embeddings):
+        batch_size, num_nodes, _ = x.size()
+        supports = F.softmax(F.relu(torch.matmul(node_embeddings, node_embeddings.transpose(-1,-2))), dim=-1)
+        support_set = [torch.eye(num_nodes).to(supports.device).unsqueeze(0).expand(batch_size, -1, -1), supports]
+        # default cheb_k = 3
+        for k in range(2, self.cheb_k):
+            support_set.append(torch.matmul(2 * supports, support_set[-1]) - support_set[-2])
+        x_g = []
+        for support in support_set:
+            x_g.append(torch.einsum("bnm,bmc->bnc", support, x))
+        x_g = torch.cat(x_g, dim=-1) # B, N, cheb_k*C
+        x_gconv = torch.einsum('bni,io->bno', x_g, self.weights) + self.bias
+        return x_gconv
+
+
 # Gated Graph Convolution
 class GatedGCN(nn.Module):
     def __init__(self, dim_in, dim_out, cheb_k, embed_dim):
@@ -36,19 +198,25 @@ class GatedGCN(nn.Module):
         self.embed_dim = embed_dim
         self.dim_in = dim_in
         self.dim_out = dim_out
+        
+        self.info_fussion = GatedFusion(embed_dim)
 
-        self.global_GCN = AGCN(dim_in, 2*dim_out, cheb_k, embed_dim) # Global Graph Convolution. (node embeddings is fixed)
-        self.local_GCN = AGCN(dim_in, dim_out, cheb_k, embed_dim) # Local Graph Convolution. (node embeddings change with input data)
+        self.global_GCN = GlobalAGCN(dim_in, dim_out, cheb_k, embed_dim) # Global Graph Convolution. (node embeddings is fixed)
+        self.local_GCN = LocalAGCN(dim_in, dim_out, cheb_k, embed_dim) # Local Graph Convolution. (node embeddings change with input data)
 
-        self.gate = nn.Linear(dim_out * 2, dim_out)
+        # self.gate = nn.Linear(dim_out * 2, dim_out)
+        self.out = nn.Linear(dim_out * 2, dim_out)
     
     def forward(self, x, global_E, local_E):
+        local_E = self.info_fussion(global_E, local_E)
+
         global_out = self.global_GCN(x, global_E)
         local_out = self.local_GCN(x, local_E)
         
-        combined = torch.cat([global_out, local_out])
-        gate_weights = torch.sigmoid(self.gate(combined))
+        combined = torch.cat([global_out, local_out], dim=-1)
+        # gate_weights = torch.sigmoid(self.gate(combined))
 
-        output = gate_weights * global_out + (1 - gate_weights) * local_out
+        # output = gate_weights * global_out + (1 - gate_weights) * local_out
+        output = self.out(combined)
 
         return output
--- model/vector_fields.py
+++ model/vector_fields.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from GCN import AGCN, GatedGCN
+from GCN import GlobalAGCN, GatedGCN, SelfAttentionLayer
 
 class FinalTanh_f(nn.Module):
     def __init__(self, input_channels, hidden_channels, hidden_hidden_channels, num_hidden_layers):
@@ -59,11 +59,13 @@ class VectorField_g(torch.nn.Module):
         if self.g_type == 'agc':
             self.node_embeddings = nn.Parameter(torch.randn(num_nodes, embed_dim), requires_grad=True)
             self.cheb_k = cheb_k
-            # self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, hidden_hidden_channels, hidden_hidden_channels))
-            # self.bias_pool = nn.Parameter(torch.FloatTensor(embed_dim, hidden_hidden_channels))
+            self.Wq = nn.Linear(hidden_hidden_channels, embed_dim)
+            self.Mem = nn.Parameter(torch.randn(128, embed_dim), requires_grad=True) # 64 is num of memory
+            nn.init.xavier_normal_(self.Mem)
+            # self.attn = SelfAttentionLayer(hidden_hidden_channels, feed_forward_dim=64, num_heads=8, dropout=0.1)
         
-        self.agcn = AGCN(hidden_hidden_channels, hidden_hidden_channels, cheb_k, embed_dim)
-
+        # self.agcn = AGCN(hidden_hidden_channels, hidden_hidden_channels, cheb_k, embed_dim)
+        self.gated_agcn = GatedGCN(hidden_hidden_channels, hidden_hidden_channels, cheb_k, embed_dim)
 
     def extra_repr(self):
         return "input_channels: {}, hidden_channels: {}, hidden_hidden_channels: {}, num_hidden_layers: {}" \
@@ -74,7 +76,12 @@ class VectorField_g(torch.nn.Module):
         z = z.relu()
 
         if self.g_type == 'agc':
-            z = self.agcn(z, self.node_embeddings)
+            # local_emb = self.attn(z)
+            query = self.Wq(z)
+            att_score = torch.softmax(torch.matmul(query, self.Mem.transpose(-1, -2)), dim=-1)
+            local_emb = torch.matmul(att_score, self.Mem)
+            # z = self.agcn(z, self.node_embeddings)
+            z = self.gated_agcn(z, self.node_embeddings, local_emb)
         else:
             raise ValueError('Check g_type argument')
 
@@ -83,31 +90,3 @@ class VectorField_g(torch.nn.Module):
         z = self.linear_out(z).view(*z.shape[:-1], self.hidden_channels, self.hidden_channels)
         z = z.tanh()
         return z #torch.Size([64, 307, 64, 1])
-
-    def agc(self, z):
-        """
-        Adaptive Graph Convolution
-        - Node Adaptive Parameter Learning
-        - Data Adaptive Graph Generation
-        """
-        node_num = self.node_embeddings.shape[0]
-        supports = F.softmax(F.relu(torch.mm(self.node_embeddings, self.node_embeddings.transpose(0, 1))), dim=1)
-        # laplacian=False
-        laplacian=False
-        if laplacian == True:
-            # support_set = [torch.eye(node_num).to(supports.device), -supports]
-            support_set = [supports, -torch.eye(node_num).to(supports.device)]
-            # support_set = [torch.eye(node_num).to(supports.device), -supports]
-            # support_set = [-supports]
-        else:
-            support_set = [torch.eye(node_num).to(supports.device), supports]
-        #default cheb_k = 3
-        for k in range(2, self.cheb_k):
-            support_set.append(torch.matmul(2 * supports, support_set[-1]) - support_set[-2])
-        supports = torch.stack(support_set, dim=0)
-        weights = torch.einsum('nd,dkio->nkio', self.node_embeddings, self.weights_pool)  #N, cheb_k, dim_in, dim_out
-        bias = torch.matmul(self.node_embeddings, self.bias_pool)                       #N, dim_out
-        x_g = torch.einsum("knm,bmc->bknc", supports, z)      #B, cheb_k, N, dim_in
-        x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
-        z = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out
-        return z
